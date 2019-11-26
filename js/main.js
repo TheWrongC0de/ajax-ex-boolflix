97c6afd06965acf714944a75e72a194b28489595
Simone <simoneugazio1993@gmail.com>
2019-11-26 16:33:47 +0100

    errore console log , non capisco perchè

--- js/main.js
+++ js/main.js
@@ -54,7 +54,9 @@ function callAjax(){
                  imglink: 'https://image.tmdb.org/t/p/w500' + elementi.poster_path,
                  titoloOriginale: elementi.original_title,
                  lingua: elementi.original_language,
-                 voto: elementi.vote_average
+                 voto: Math.ceil(elementi.vote_average),
+                 flag: creazioneBandiera(elementi.original_language),
+                 stella: creazioneStelle(Math.ceil(elementi.vote_average))
               };
               //Creounavar per l'html dove appariranno i film
               var html = template(globalFilm);
@@ -71,3 +73,49 @@ function callAjax(){
         }
       })
 }
+
+/*
+Trasformiamo il numero da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+Qui un esempio di chiamata per le serie tv:
+https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+ */
+
+//Creo funzioni per sostituire stelle e bandiere
+
+ function creazioneStelle(voto){
+
+   var voto = voto/2;
+
+   var stella = "";
+
+   for(var i=1; i<=5;i++){
+      if(i<=voto){
+         stella += '<i class="fas fa-star"></i>';
+
+      } else{
+         stella += '<i class="far fa-star"></i>';
+      }
+   }
+   return stella;
+};
+
+
+function creazioneBandiera(flag){
+   var bandieraimg;
+
+   switch (flag) {
+
+      case "it":
+            bandieraimg = "<img src='img/eu.jpg' width='30px'>";
+         break;
+      case "usa":
+            bandieraimg = "<img src='img/usa.png' width='30px'>";
+         break;
+      default:
+         bandieraimg = "<img src='img/terra.jpg' width='30px'>";
+   }
+   return bandieraimg;
+};
